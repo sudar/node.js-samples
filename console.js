9517b980cf9fc220a33adccd1abc4d8872d26567
Sudar <sudar@sudarmuthu.com>
2011-09-30 21:24:45 +0530

    Added console.js, explaning the features of console module

---
+++ console.js
@@ -0,0 +1,18 @@
+// Basic logging
+
+console.log("Basic string logging");
+// Inspecting variables
+
+
+var obj = {1: true, 2: false};
+console.log(obj);
+
+
+// Using string interpoloation
+console.log("This is a number: %d, and this is a string: %s, and this is a JSON object: %j", 42, 'Hello', obj);
+
+// Warning strings
+console.log("Warn");
+
+// to print stack trace
+console.trace();
